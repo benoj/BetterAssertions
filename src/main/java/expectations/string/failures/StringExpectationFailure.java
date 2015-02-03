421c4e5cc0d1c07f5c2c6e30414f3d864364e2f5
benoj <ben.j.flowers@gmail.com>
2015-02-03 09:39:49 +0000

    Renamed class in hope that git will not freak out

--- src/main/java/expectations/string/failures/StringExpectationFailure.java
+++ src/main/java/expectations/string/failures/StringExpectationFailure.java
@@ -1,10 +1,10 @@
 package expectations.string.failures;
 
-import expectations.string.failures.messageBuilders.StringFailureMessageBuilder;
+import expectations.string.failures.messageBuilders.StringExpectationFailureMessageBuilder;
 
 abstract class StringExpectationFailure extends Exception {
     public StringExpectationFailure(String actual, String expected, String condition) {
-        super(StringFailureMessageBuilder.build(actual, expected, condition));
+        super(StringExpectationFailureMessageBuilder.build(actual, expected, condition));
     }
 }
 
--- src/main/java/expectations/string/failures/messageBuilders/StringExpectationFailureMessageBuilder.java
+++ src/main/java/expectations/string/failures/messageBuilders/StringExpectationFailureMessageBuilder.java
@@ -1,6 +1,6 @@
 package expectations.string.failures.messageBuilders;
 
-public class StringFailureMessageBuilder {
+public class StringExpectationFailureMessageBuilder {
     public static String build(String actual, String expected, String condition){
         return "Expected: '" + actual + "' to " + condition + " '" + expected + "'";
     }
